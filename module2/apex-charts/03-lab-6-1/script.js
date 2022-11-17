2366b0dfea7a65d0ba2069ad68322c28e1cd7e7d
immalcolm <malcolm_yam@np.edu.sg>
2022-11-17 11:22:10 +0800

    switch to scatter chart

---
+++ module2/apex-charts/03-lab-6-1/script.js
@@ -0,0 +1,32 @@
+const DATA_URL =
+  "https://raw.githubusercontent.com/immalcolm/data-and-misc/main/data-graph.json";
+//setup options
+const options = {
+  chart: {
+    type: "scatter",//cos line doesnt make any sense
+    height: "100%",
+  },
+  series: [],
+  noData: {
+    text: "loading....",
+  },
+};
+
+async function loadData() {
+  const response = await axios.get(DATA_URL);
+  console.log(response.data);
+
+  //dont forget to return some info back to our function call
+  return response.data;
+}
+
+window.addEventListener("DOMContentLoaded", async function () {
+  let series = await loadData();
+  chart.updateSeries([{ name: "Temp", data: series.temperatures }]);
+
+});
+
+const divChart = document.querySelector("#chart");
+const chart = new ApexCharts(divChart, options);
+
+chart.render();
