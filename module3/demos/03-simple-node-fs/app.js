c9f8bd51652f4e9de4480159c4cbe170bd91f041
immalcolm <malcolm_yam@np.edu.sg>
2022-11-17 15:12:38 +0800

    added comments on pathing n slashes in require

--- module3/demos/03-simple-node-fs/app.js
+++ module3/demos/03-simple-node-fs/app.js
@@ -1,5 +1,9 @@
 //work with file system
 //using Node Global fs 
+
+//relative pathing './<path>'
+//absolute pathing '/<path>'
+//if you never include the slash, we will assume it's a global module
 const fs = require('fs');
 
 //write sample text to panda.txt
@@ -10,8 +14,8 @@ fs.readFile('./panda.json', (err, data)=>{
     console.log(data);
 });
 
-
 fs.readFile('./panda.json', 'utf-8', (err, data)=>{
     console.log(data);
 });
+console.log('test');
 
