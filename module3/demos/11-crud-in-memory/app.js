3133855801bf28cc7265e785cb23f2b9d812218a
immalcolm <malcolm_yam@np.edu.sg>
2022-11-22 17:37:34 +0800

    updates to create for crud

--- module3/demos/11-crud-in-memory/app.js
+++ module3/demos/11-crud-in-memory/app.js
@@ -34,14 +34,15 @@ let foodRecords = [
         foodName: "Hawaiian Pizza",
         calories: 500,
         meal: "dinner",
-        tags: ["organic","western"],
+        tags: ["organic","cheap"],
     },
 ]
 
 //CRUD
 //Create, Read, Update, Delete
 
-//R: read - to retrieve and display data from somewhere (db, localstorage, in-memory, etc)
+//R: read - to retrieve and display data from somewhere 
+//(db, localstorage, in-memory, etc)
 //use a url that matches the purpose? 
 //SEO - search engine optimisation reasons + syntax friendly
 app.get("/all-food", function(req, res){
@@ -50,6 +51,50 @@ app.get("/all-food", function(req, res){
         allFood: allFood
     })
 });
+
+
+//C: create
+//2 routes 
+//1- to display form
+//1 - handle all form processing
+app.get("/add-food", function(req, res){
+    res.render("add-food");
+});
+
+//C: create to create form content
+app.post("/add-food", function(req,res){
+    //res.send("received");
+    let foodName = req.body.foodName;
+    let calories = req.body.calories;
+    let meal = req.body.meal || "western";
+    
+    let selectedTags = [];
+    if(req.body.tags){
+        if(Array.isArray(req.body.tags)){
+            selectedTags = req.body.tags;
+        }else{
+            //if only 1 selected
+
+            //selectedTags = [req.body.tags];
+            selectedTags.push(req.body.tags);
+        }
+    }
+
+    let newFoodRecord = {
+        "id": Math.floor(Math.random() * 10000 +1),
+        "foodName": foodName,
+        "calories": calories,
+        "meal": meal,
+        "tags": selectedTags
+    }
+
+    foodRecords.push(newFoodRecord);
+
+    console.log(`New item is : ${newFoodRecord.id}`);
+    //redirect user to page
+    res.redirect("/all-food");
+
+})
 
 //ROUTING
 app.get("/", function(req, res){
